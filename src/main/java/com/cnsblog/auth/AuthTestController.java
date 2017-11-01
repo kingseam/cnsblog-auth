16a1c1111cf33ab3246cfcca5444389e35db6b31
164114 <164114@shinsegae.com>
2017-11-01 17:50:16 +0900

    + URI / add

--- src/main/java/com/cnsblog/auth/AuthTestController.java
+++ src/main/java/com/cnsblog/auth/AuthTestController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthTestController {
-	
+
+	@RequestMapping("/")
+	public String main() {
+		return "JWT Auth Server!!";
+	}
+
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello auth";
 	}
-	
+
 	@RequestMapping("/api/hello")
 	public String hello2() {
 		return "Hello api auth";
