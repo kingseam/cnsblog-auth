d1923329c9747d4d4a8362a5b99459ccd2767256
164114 <164114@shinsegae.com>
2017-11-14 12:25:56 +0900

    - cors permit

--- src/main/java/com/cnsblog/auth/AuthorizationSeverConfig.java
+++ src/main/java/com/cnsblog/auth/AuthorizationSeverConfig.java
@@ -94,7 +94,4 @@ public class AuthorizationSeverConfig extends AuthorizationServerConfigurerAdapt
 		*/
 
 	}
-
-
-
 }
--- src/main/java/com/cnsblog/auth/SecurityConfig.java
+++ src/main/java/com/cnsblog/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.cnsblog.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -40,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
 	public void configure(HttpSecurity http) throws Exception {
-    	http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+    	http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().antMatchers("/**").permitAll();
 	}
 
 
